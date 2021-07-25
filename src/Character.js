f16145771522eb735b2c60f1c0f590c47c0a9576
Dylan-Allison-Damitz <dylandamitz@gmail.com>
2021-07-25 11:39:21 -0700

    add Constructor.js with 4 object parameters

---
+++ src/Character.js
@@ -0,0 +1,8 @@
+export default class Character {
+  constructor(factionName, questPoints, battlePoints, totalPoints) {
+    this.factionName = factionName;
+    this.questPoints = questPoints;
+    this.battlePoints = battlePoints;
+    this.totalPoints =  totalPoints;
+  }
+}
