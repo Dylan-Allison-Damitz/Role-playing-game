f419c56f689f724c864d6b9bf5d1c41f48b678a5
Dylan-Allison-Damitz <dylandamitz@gmail.com>
2021-07-25 12:33:26 -0700

    add addQuestPoints prototype

--- src/Character.js
+++ src/Character.js
@@ -1,8 +1,12 @@
 export default class Character {
   constructor(factionName, questPoints, battlePoints, totalPoints) {
     this.factionName = factionName;
-    this.questPoints = questPoints;
+    this.questPoints = 0;
     this.battlePoints = battlePoints;
     this.totalPoints =  totalPoints;
   }
+}
+  Character.prototype.addQuestPoints = function() {
+    this.questPoints += 1;
+    return this.questPoints;
 }
